766affbea5f04f1c33bcb31db3224e6d2ab48551
Elona <eloniuke@gmail.com>
2018-04-18 09:59:35 +0200

    -

--- Sandbox/Sensors/ThermalBox/main.cpp
+++ Sandbox/Sensors/ThermalBox/main.cpp
@@ -15,54 +15,54 @@ Mat frame;
 
 int main(int, char** argv)
 {
-//Capturing image form the thermal camera
-    VideoCapture cap("http://169.254.228.255/mjpg/video.mjpg?");
-if(!cap.isOpened())
-    {
-        cout<<"Camera not found"<<endl;
-        getchar();
-        return -1;
-    }
-while ( cap.isOpened() )
-    {
-        //Put the captured image in the matrix "frame"
-        cap >> frame;
+	//Capturing image form the thermal camera
+		VideoCapture cap("http://169.254.228.255/mjpg/video.mjpg?");
+	if(!cap.isOpened())
+		{
+			cout<<"Camera not found"<<endl;
+			getchar();
+			return -1;
+		}
+	while ( cap.isOpened() )
+		{
+			//Put the captured image in the matrix "frame"
+			cap >> frame;
 
-  	cvtColor( frame, frame, CV_BGR2GRAY ); //Convert the image to grayscale
+  			cvtColor( frame, frame, CV_BGR2GRAY ); //Convert the image to grayscale
   	
-  	//Thresholdin the imgae and converting it to binary & Median blur
-  	   	 threshold( frame, frame, 40,255,THRESH_BINARY);
-  	 medianBlur(frame, frame, 5);
+  			//Thresholding the imgae and converting it to binary & Median blur
+  	   		threshold( frame, frame, 40,255,THRESH_BINARY);
+  			medianBlur(frame, frame, 5);
 
-  	 //Storing the points and details of the contours in vectors. Hierarchy = Image topology
-	 vector<vector<Point> > contours;
-  		vector<Vec4i> hierarchy;
-  		//Function to find contours
-  		findContours( frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-  		cvtColor( frame, frame, CV_GRAY2RGB); //Converting the image back to RGB so we can have colourful rectangles
+  			//Storing the points and details of the contours in vectors. Hierarchy = Image topology
+			vector<vector<Point> > contours;
+  			vector<Vec4i> hierarchy;
+  			//Function to find contours
+  			findContours( frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  			cvtColor( frame, frame, CV_GRAY2RGB); //Converting the image back to RGB so we can have colourful rectangles
 
-  		//Giving it an input vector
-  		vector<vector<Point> > contours_poly(contours.size() );
-		vector<Rect> boundRect(contours.size() );
+  			//Giving it an input vector
+  			vector<vector<Point> > contours_poly(contours.size() );
+			vector<Rect> boundRect(contours.size() );
   		
-		for( int i = 0; i < contours.size(); i++ )
-   { 
-    approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true ); //contours_poly[i] --> Making sure the rectangles follow our contours
-    boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-   }
+			for( int i = 0; i < contours.size(); i++ )
+			{ 
+			approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true ); //contours_poly[i] --> Making sure the rectangles follow our contours
+			boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+			}
 
-  for( int i = 0; i< contours.size(); i++ ) //Loop for setting the the colour and drawing the rectangles
-   {
-    Scalar color = Scalar(0,0, 255); //red
-    Scalar color1 = Scalar(255,0, 0); //blue
-    rectangle(frame, boundRect[i].tl(), boundRect[i].br(), color1, 2, 8, 0 );
-    drawContours(frame, contours, i, color, 2, 8, hierarchy, 0, Point() );
-   }
+			for( int i = 0; i< contours.size(); i++ ) //Loop for setting the the colour and drawing the rectangles
+			{
+			Scalar color = Scalar(0,0, 255); //red
+			Scalar color1 = Scalar(255,0, 0); //blue
+			rectangle(frame, boundRect[i].tl(), boundRect[i].br(), color1, 2, 8, 0 );
+			drawContours(frame, contours, i, color, 2, 8, hierarchy, 0, Point() );
+			}
   		
-//Show the image
-        imshow("sourceimg", frame);
-         if(frame.empty()) break;
- 	if(waitKey(30) >= 0) break;
-    } 
-    return 0;
+			//Show the image
+			imshow("sourceimg", frame);
+				if(frame.empty()) break;
+ 			if(waitKey(30) >= 0) break;
+		} 
+		return 0;
 }
