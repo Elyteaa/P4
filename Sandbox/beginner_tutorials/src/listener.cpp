3860fd84d2194c21e2eae81b15e24d02be7c292d
Elona <eloniuke@gmail.com>
2018-04-23 14:34:16 +0200

    merged

--- Sandbox/beginner_tutorials/src/listener.cpp
+++ Sandbox/beginner_tutorials/src/listener.cpp
@@ -1,5 +1,75 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <iostream>
+#include <geometry_msgs/Twist.h>
+
+class RobotDriver
+{
+private:
+	//! The node handle we'll be using
+	ros::NodeHandle nh_;
+	//! We will be publishing to the "/base_controller/command" topic to issue commands
+	ros::Publisher cmd_vel_pub_;
+
+public:
+	//! ROS node initialization
+	RobotDriver(ros::NodeHandle &nh)
+	{
+		nh_ = nh;
+		//set up the publisher for the cmd_vel topic
+		cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+	}
+
+	//! Loop forever while sending drive commands based on keyboard input
+	bool driveKeyboard()
+	{		/*std::cout << "Type a command and then press enter.  "
+			"Use '+' to move forward, 'l' to turn left, "
+			"'r' to turn right, '.' to exit.\n";*/
+
+		std::cout << "f + ENTER to move forward";
+
+		//we will be sending commands of type "twist"
+		geometry_msgs::Twist base_cmd;
+
+		char cmd[50];
+		while (nh_.ok()) {
+
+			std::cin.getline(cmd, 50);
+			if (cmd[0] != 'f')
+			{
+				std::cout << "unknown command:" << cmd << "\n";
+				continue;
+			}
+
+			base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
+			//move forward
+			//if (
+//cmd[0] == 'f'
+//) {
+				base_cmd.linear.x = 0.25;
+			//}
+			/*//turn left (yaw) and drive forward at the same time
+			else if (cmd[0] == 'l') {
+				base_cmd.angular.z = 0.75;
+				base_cmd.linear.x = 0.25;
+			}
+			//turn right (yaw) and drive forward at the same time
+			else if (cmd[0] == 'r') {
+				base_cmd.angular.z = -0.75;
+				base_cmd.linear.x = 0.25;
+			}*/
+			//quit
+			//else if (cmd[0] == '.') {
+			//	break;
+			//}
+
+			//publish the assembled command
+			cmd_vel_pub_.publish(base_cmd);
+		}
+		return true;
+	}
+
+};
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
@@ -22,7 +92,8 @@ int main(int argc, char **argv)
    * part of the ROS system.
    */
   ros::init(argc, argv, "listener");
-
+  ros::NodeHandle nh;
+  RobotDriver driver(nh);
   /**
    * NodeHandle is the main access point to communications with the ROS system.
    * The first NodeHandle constructed will fully initialize this node, and the last
