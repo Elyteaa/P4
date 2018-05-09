2d9e621a36d0dd465c6d975e695723a61ae094fa
Elona <eloniuke@gmail.com>
2018-05-09 15:04:57 +0200

    -

--- Project/beginner_tutorials/src/asus.cpp
+++ Project/beginner_tutorials/src/asus.cpp
@@ -1,16 +1,38 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Int32MultiArray.h"
 
-void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
+void sensor(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-ROS_INFO("Angle [%f] and distance [%f]", msg->angle_min, msg->ranges[0]);
+	for (int i = 0; i < 640; i++) {
+		distance[i] = msg->ranges[i];
+	}
+	//ROS_INFO("Angle [%f] and distance [%f]", msg->angle_min, msg->ranges[0]);
 }
 
-int main(int argc, char **argv){
-ros::init(argc,argv,"listener");
-ros::NodeHandle n;
-ros::Subscriber sub = n.subscribe("chatter", 100, callback);
+void humans(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+	for (int i = 0; i < 640; i++) {
+		distance[i] = msg->ranges[i];
+	}
+	//ROS_INFO("Angle [%f] and distance [%f]", msg->angle_min, msg->ranges[0]);
+}
 
-ros::spin();
-return 0;
+int distance[640];
+
+int main(int argc, char **argv){
+	ros::init(argc,argv,"asus_camera");
+	ros::NodeHandle nh;
+	//ros::Publisher pub = nh.advertise<std_msgs::String>("/thermal", 10);
+	ros::Subscriber sub = nh.subscribe("/scan", 10, sensor);
+	ros::Subscriber sub2 = nh.subscribe("/thermalHumans", 10);
+
+	std_msgs::Int32MultiArray;1
+
+	ros::Rate loop_rate(10);
+
+
+
+	ros::spin();
+	return 0;
 }
