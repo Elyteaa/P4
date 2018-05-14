817bb1e66ad0cdc36dc0abeeadb890e1d2a7b79d
Elona <eloniuke@gmail.com>
2018-05-14 10:56:56 +0200

    preparing the node

--- Project/beginner_tutorials/src/carsdetect.cpp
+++ Project/beginner_tutorials/src/carsdetect.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
+#include "ros/ros.h"
+#include "sensor_msgs/Image"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-
+#include <stdlib.h>
 #include <stdio.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
 
 using namespace std;
 using namespace cv;
