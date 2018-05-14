c7960755c90f92cdf3245aef09035a879c061b4d
Elona <eloniuke@gmail.com>
2018-05-14 15:24:32 +0200

    rgb node

--- Project/beginner_tutorials/src/carsdetect.cpp
+++ Project/beginner_tutorials/src/carsdetect.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "ros/ros.h"
-#include "sensor_msgs/Image"
+#include "sensor_msgs/Image.h"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -14,7 +14,49 @@ using namespace std;
 using namespace cv;
 using namespace cv_bridge;
 
+/** Global variables */
+String face_cascade_name, eyes_cascade_name;
+CascadeClassifier face_cascade; //faces = cars
+CascadeClassifier eyes_cascade; //eyes = bikes
+String window_name = "Capture - Car detection";
+Mat frame;
 static const string OPENCV_WINDOW = "Image window";
+int argc;
+char** argv;
+
+/** Function Headers */
+void detectAndDisplay(Mat frame)
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+
+	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(60, 60));
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 3, 8, 0);
+
+		Mat faceROI = frame_gray(faces[i]);
+		std::vector<Rect> eyes;
+
+		//-- In each face, detect eyes
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+		for (size_t j = 0; j < eyes.size(); j++)
+		{
+			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+			circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
+		}
+	}
+	//-- Show what you got
+	imshow(window_name, frame);
+}
 
 class ImageConverter
 {
@@ -42,6 +84,7 @@ public:
 
 	void imageCb(const sensor_msgs::ImageConstPtr& msg)
 	{
+		ros::Rate loop_rate(10);
 		cv_bridge::CvImagePtr cv_ptr;
 		try
 		{
@@ -73,8 +116,8 @@ public:
 			VideoCapture capture;
 
 			//-- 1. Load the cascades
-			if (!face_cascade.load("/home/drawn/opencv/data/haarcascades/cars.xml")) { printf("--(!)Error loading face cascade\n"); return -1; };
-			if (!eyes_cascade.load("/home/drawn/opencv/data/haarcascades/bike.xml")) { printf("--(!)Error loading eyes cascade\n"); return -1; };
+			if (!face_cascade.load("/home/drawn/opencv/data/haarcascades/cars.xml")) { printf("--(!)Error loading face cascade\n"); };
+			if (!eyes_cascade.load("/home/drawn/opencv/data/haarcascades/bike.xml")) { printf("--(!)Error loading eyes cascade\n"); };
 
 			//-- 2. Read the video stream //load the video
 			//capture.open(0);
@@ -89,7 +132,7 @@ public:
 				}*/
 
 				//-- 3. Apply the classifier to the frame
-			detectAndDisplay(cv_ptr);
+			detectAndDisplay(cv_ptr->image);
 
 			if (waitKey(10) == 27) { break; } // escape
 
@@ -102,110 +145,66 @@ public:
 			ros::spinOnce();
 			loop_rate.sleep();
 		}
-		}
-	}
+	};
 };
 
-/** Function Headers */
-void detectAndDisplay(cv_bridge::CvImagePtr frame );
-void msgCallback(const sensor_msgs::Image::ConstPtr& msg);
+	//void msgCallback(const sensor_msgs::Image::ConstPtr& msg);
 
-/** Global variables */
-String face_cascade_name, eyes_cascade_name;
-CascadeClassifier face_cascade; //faces = cars
-CascadeClassifier eyes_cascade; //eyes = bikes
-String window_name = "Capture - Car detection";
-Mat frame;
-
-/** @function main */
-int main( int argc, const char** argv )
-{
-	ros::init(argc, argv, "RGB_node");
-	ros::NodeHandle n;
-
-	ros::Rate loop_rate(10);
-
-	ImageConverter ic;
-
-	/*while (ros::ok())
+	/** @function main */
+	int main()
 	{
-		CommandLineParser parser(argc, argv,
-			"{help h||}"
-			"{face_cascade|../../data/haarcascades/cars.xml|}"
-			"{eyes_cascade|../../data/haarcascades/bike.xml|}");
+		ros::init(argc, argv, "RGB_node");
+		ros::NodeHandle n;
 
-		parser.about("\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
-			"You can use Haar or LBP features.\n\n");
-		parser.printMessage();
+		ImageConverter ic;
 
-		face_cascade_name = parser.get<String>("face_cascade");
-		eyes_cascade_name = parser.get<String>("eyes_cascade");
-		VideoCapture capture;
-
-		//-- 1. Load the cascades
-		if (!face_cascade.load("/home/drawn/opencv/data/haarcascades/cars.xml")) { printf("--(!)Error loading face cascade\n"); return -1; };
-		if (!eyes_cascade.load("/home/drawn/opencv/data/haarcascades/bike.xml")) { printf("--(!)Error loading eyes cascade\n"); return -1; };
-
-		//-- 2. Read the video stream //load the video
-		capture.open(0);
-		if (!capture.isOpened()) { printf("--(!)Error opening video capture\n"); return -1; }
-
-		while (capture.read(frame))
+		/*while (ros::ok())
 		{
-			if (frame.empty())
+			CommandLineParser parser(argc, argv,
+				"{help h||}"
+				"{face_cascade|../../data/haarcascades/cars.xml|}"
+				"{eyes_cascade|../../data/haarcascades/bike.xml|}");
+
+			parser.about("\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
+				"You can use Haar or LBP features.\n\n");
+			parser.printMessage();
+
+			face_cascade_name = parser.get<String>("face_cascade");
+			eyes_cascade_name = parser.get<String>("eyes_cascade");
+			VideoCapture capture;
+
+			//-- 1. Load the cascades
+			if (!face_cascade.load("/home/drawn/opencv/data/haarcascades/cars.xml")) { printf("--(!)Error loading face cascade\n"); return -1; };
+			if (!eyes_cascade.load("/home/drawn/opencv/data/haarcascades/bike.xml")) { printf("--(!)Error loading eyes cascade\n"); return -1; };
+
+			//-- 2. Read the video stream //load the video
+			capture.open(0);
+			if (!capture.isOpened()) { printf("--(!)Error opening video capture\n"); return -1; }
+
+			while (capture.read(frame))
 			{
-				printf(" --(!) No captured frame -- Break!");
-				break;
+				if (frame.empty())
+				{
+					printf(" --(!) No captured frame -- Break!");
+					break;
+				}
+
+				//-- 3. Apply the classifier to the frame
+				detectAndDisplay(frame);
+
+				if (waitKey(10) == 27) { break; } // escape
 			}
+			ros::spinOnce();
+			loop_rate.sleep();
+		}*/
+		return 0;
+	}
 
-			//-- 3. Apply the classifier to the frame
-			detectAndDisplay(frame);
-
-			if (waitKey(10) == 27) { break; } // escape
-		}
-		ros::spinOnce();
-		loop_rate.sleep();
+	/*void msgCallback(const sensor_msgs::Image::ConstPtr& msg)
+	{
+		cout << "function entered" << endl;
+		ROS_INFO("height = %d, width = %d", msg->height, msg->width);
+		//frame = msg->data;
 	}*/
-    return 0;
-}
 
-/*void msgCallback(const sensor_msgs::Image::ConstPtr& msg)
-{
-	cout << "function entered" << endl;
-	ROS_INFO("height = %d, width = %d", msg->height, msg->width);
-	//frame = msg->data;
-}*/
-
-/** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
-{
-    std::vector<Rect> faces;
-    Mat frame_gray;
-	
-    cvtColor( frame->image, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
-
-    //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(60, 60) );
-
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-        ellipse( frame->image, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 3, 8, 0 );
-
-        Mat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
-
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame->image, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-        }
-    }
-    //-- Show what you got
-    imshow( window_name, frame->image );
-}
+	/** @function detectAndDisplay */
