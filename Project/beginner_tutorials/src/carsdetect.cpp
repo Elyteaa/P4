ba75165b54b4b48d01265debcdcdf78067fb7c4b
Elona <eloniuke@gmail.com>
2018-05-15 13:33:09 +0200

    rgb

--- Project/beginner_tutorials/src/carsdetect.cpp
+++ Project/beginner_tutorials/src/carsdetect.cpp
@@ -61,16 +61,13 @@ void detectAndDisplay(Mat frame)
 
 class ImageConverter
 {
-private:
 	ros::NodeHandle nh_;
 	image_transport::ImageTransport it_;
 	image_transport::Subscriber image_sub_;
 	image_transport::Publisher image_pub_;
-	int argc2;
-	const char** argv2;
 
 public:
-	/*ImageConverter()
+	ImageConverter()
 		: it_(nh_)
 	{
 		// Subscrive to input video feed and publish output video feed
@@ -84,14 +81,6 @@ public:
 	~ImageConverter()
 	{
 		destroyWindow(OPENCV_WINDOW);
-	}*/
-
-	ImageConverter(ros::NodeHandle &nh)
-		: it_(nh_)
-	{
-		image_sub_ = it_.subscribe("/camera/image_raw", 1,
-			&ImageConverter::imageCb, this);
-		image_pub_ = it_.advertise("/image_converter/output_video", 1);
 	}
 
 	void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -107,62 +96,19 @@ public:
 			return;
 		}
 
-		our_frame = cv_ptr;
-
-		//ImageConverter::rgb(cv_ptr);
-
-		/*// Draw an example circle on the video stream
+		// Draw an example circle on the video stream
 		if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-			circle(cv_ptr->image, Point(50, 50), 10, CV_RGB(255, 0, 0));*/
-	};
+			circle(cv_ptr->image, Point(50, 50), 10, CV_RGB(255, 0, 0));
 
-	void rgb() {
-		ros::Rate loop_rate(10);
-		while (ros::ok())
-		{
-			CommandLineParser parser(argc2, argv2,
-				"{help h||}"
-				"{face_cascade|../../data/haarcascades/cars.xml|}"
-				"{eyes_cascade|../../data/haarcascades/bike.xml|}");
+		cout << "It has been read" << endl;
 
-			parser.about("\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
-				"You can use Haar or LBP features.\n\n");
-			parser.printMessage();
+		// Update GUI Window
+		imshow(OPENCV_WINDOW, cv_ptr->image);
+		our_frame = cv_ptr;
+		waitKey(5);
 
-			face_cascade_name = parser.get<String>("face_cascade");
-			eyes_cascade_name = parser.get<String>("eyes_cascade");
-			VideoCapture capture;
-
-			//-- 1. Load the cascades
-			if (!face_cascade.load("/home/drawn/opencv/data/haarcascades/cars.xml")) { printf("--(!)Error loading face cascade\n"); };
-			if (!eyes_cascade.load("/home/drawn/opencv/data/haarcascades/bike.xml")) { printf("--(!)Error loading eyes cascade\n"); };
-
-			//-- 2. Read the video stream //load the video
-			//capture.open(0);
-			//if (!capture.isOpened()) { printf("--(!)Error opening video capture\n"); return -1; }
-
-			/*while (capture.read(frame))
-			{
-			if (frame.empty())
-			{
-			printf(" --(!) No captured frame -- Break!");
-			break;
-			}*/
-
-			//-- 3. Apply the classifier to the frame
-			detectAndDisplay(our_frame->image);
-
-			if (waitKey(10) == 27) { break; } // escape
-
-											  // Update GUI Window
-			imshow(OPENCV_WINDOW, our_frame->image);
-
-			// Output modified video stream
-			image_pub_.publish(our_frame->toImageMsg());
-
-			ros::spinOnce();
-			loop_rate.sleep();
-		}
+		// Output modified video stream
+		image_pub_.publish(cv_ptr->toImageMsg());
 	}
 };
 
@@ -173,12 +119,14 @@ public:
 	{
 		ros::init(argc, argv, "RGB_node");
 		ros::NodeHandle n;
+		ros::Rate loop_rate(10);
 
-		ImageConverter ic(n);
-		ic.rgb();
+		cout << "Class init" << endl;
+		ImageConverter ic;
 
-		/*while (ros::ok())
+		while (ros::ok())
 		{
+			cout << "In the loop" << endl;
 			CommandLineParser parser(argc, argv,
 				"{help h||}"
 				"{face_cascade|../../data/haarcascades/cars.xml|}"
@@ -215,7 +163,7 @@ public:
 			}
 			ros::spinOnce();
 			loop_rate.sleep();
-		}*/
+		}
 		return 0;
 	}
 
