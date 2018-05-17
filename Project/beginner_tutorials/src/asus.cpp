06936c99ed394867439c6f6e8cc871b060c5da89
Elona <eloniuke@gmail.com>
2018-05-17 10:40:42 +0200

    Cleaned and commented; NEEDS FIXING
    Check comments in asus.cpp

--- Project/beginner_tutorials/src/asus.cpp
+++ Project/beginner_tutorials/src/asus.cpp
@@ -8,30 +8,27 @@
 #include "std_msgs/Int32.h"
 #include "std_msgs/Float32MultiArray.h"
 
-//using namespace std;
-
+//Array saves data from one full laser scan (58 degrees)
 float distance[640];
 int x, y;
 std::vector<int> cars;
 
+//The function saves laser scan data to a global variable, which will be used later
 void sensor(const sensor_msgs::LaserScan::ConstPtr& msgs)
 {
 	for (int i = 0; i < 640; i++) {
 		distance[i] = msgs->ranges[i];
-		//std::cout << distance[i] << std::endl;
 	}
-	//ROS_INFO("Angle [%f] and distance [%f]", msgs->angle_min, msgs->ranges[0]);
-	//std::cout << "Inside the function: " << distance[7] << std::endl;
 }
 
+//Function saves positions of detected humans, sent by the thermal camera node
 void humans(const std_msgs::Int32MultiArray::ConstPtr& msgh)
 {
 	x = msgh->data[0];
 	y = msgh->data[1];
-	//ROS_INFO("Angle [%f] and distance [%f]", msg->angle_min, msg->ranges[0]);
-	//std::cout << "Values are as follows: " << x << " " << y << std::endl;
 }
 
+//Function saves positions of detected cars, sent by the RGB camera node
 void carsCallback(const std_msgs::Int32MultiArray::ConstPtr& message)
 {
 	cars.clear();
@@ -52,44 +49,45 @@ int main(int argc, char **argv)
 	ros::Subscriber sub2 = nh.subscribe("/thermalHumans", 10, humans);
 	ros::Subscriber sub3 = nh.subscribe("/rgbCars", 10, carsCallback);
 
-	std_msgs::Int32MultiArray msgh;
 	sensor_msgs::LaserScan msgs;
+	std_msgs::Int32MultiArray msgh;
 	std_msgs::Int32 turtle;
 	std_msgs::Float32MultiArray human_dis;
 
 	ros::Rate loop_rate(10);
 
-	//whole thermal image is in LaserScan readings between 171 and 477
-
+	//Thermal image is in LaserScan readings between 171 and 477
+	//RGB image is in all LaserScan readings, there is a margin of missing data
 	while (ros::ok())
 	{
 		float min = 0.0;
 		float min2 = 0.0;
 		bool go = true;
 		human_dis.data.clear();
-		//std::cout << "In the while loop before ros::ok" << std::cout;
-		//if(ros::ok()){
-		//std::cout << "Inside the while loop: " << distance[6] << std::endl;
-		//for (int i = x+171; i < y; i + 2) {
+
+		//The loop checks each laser scan for appropriote attributes
 		for(int i = 0; i < 640; i++)
 		{
-			if (y != 0)
+			//If there were any humans detected and the current laser scan element is within the human's position:
+			if ((y != 0) && (i >= x + 171) && (i <= y + 171))
 			{
-				if ((i >= x + 171) && (i <= y + 171))//&& (distance[i] <=2)) //if it's a range, where a human has been detected
+				//NEEDS FIXING
+				//Finding distance to the humans:
+				min = distance[x + 171];
+				if ((distance[i] < min) && (distance[i] != 0))
 				{
-					min = distance[x + 171];
-					if ((distance[i] < min) && (distance[i] != 0))
-					{
-						min = distance[i];
-					}
-					if ((distance[i] <= 2) && (distance[i] != 0))
-					{
-						std::cout << "A human is closer than 2 meters" << std::endl;
-						turtle.data = 0;
-						pub.publish(turtle); //message sent to the turtlebot to stop
-					}
+					min = distance[i];
+				}
+				if ((distance[i] <= 2) && (distance[i] != 0))
+				{
+					std::cout << "A human is closer than 2 meters" << std::endl;
+					//In case a human is detected closer than two meters, the TurtleBot is commanded to stop
+					turtle.data = 0;
+					pub.publish(turtle);
 				}
 			}
+
+			//NEEDS FIXING
 			/*if (cars.size() != 0) {
 				for (int j = 0; j < cars.size(); j + 2) {
 					if ((i >= x + 171) && (i <= y + 171))//&& (distance[i] <=2)) //if it's a range, where a human has been detected
@@ -108,35 +106,36 @@ int main(int argc, char **argv)
 					}
 				}
 			}*/
+
+			//Checks for obstacles withing 28 degree angle ahead; if any detected, command is sent to stop, as a result
+			//the TurtleBot will wait until the way is cleared for at least 1.2 meters ahead
 			if (go && (i >= 171)&&(i <= 477)&& (distance[i] <= 1.2) && (distance[i] != 0))
 			{
 					go = false;
 					turtle.data = 0;
 					pub.publish(turtle);
-					std::cout << "There's an obsticle closer than 1.2 meters" << std::endl;
-					std::cout << "Reading number: " << i << std::endl;
 				}
+
+			//If no obstacles have been detected, the TurtleBot is commanded to go forward
 			if (go)
 			{
 				turtle.data = 1;
-				//std::cout << "There are no obstacles within 1 m in the moving direction" << std::endl;
 				pub.publish(turtle);
 			}
 		}
-			//std::cout << i << " " << distance[i] << std::endl; 
-		//std::cout << x << " " << y << std::endl;
+
+		//NEEDS FIXING; THIS IS RELATED TO THE DISTANCE OF HUMANS
 		if (y != 0)
 		{
 			human_dis.data.push_back(x);
 			human_dis.data.push_back(min);
 			pub2.publish(human_dis);
 		}
+
 		x = 0;
 		y = 0;
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
-
-	//ros::spin();
 	return 0;
 }
