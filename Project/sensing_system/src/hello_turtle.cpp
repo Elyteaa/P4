a26dbac910243fb1f06dc7a948aa49b7b84b5b52
hvsmrtn <mhavas16@student.aau.dk>
2018-05-23 10:16:07 +0200

    Add files via upload

---
+++ Project/sensing_system/src/hello_turtle.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include "std_msgs/Int32.h"
+#include <unistd.h>
+
+using namespace std;
+
+int turtle_move_command;
+unsigned int microseconds = 100;
+
+class RobotDriver
+{
+private:
+	ros::NodeHandle nh_;
+	ros::Publisher cmd_vel_pub_;
+
+public:
+	//ROS node initialization
+	RobotDriver(ros::NodeHandle &nh)
+	{
+		nh_ = nh;
+		cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+	}
+
+	//Loop forever; receiving commands from the /turtleCommands topic
+	bool driveTurtle()
+	{
+		//we will be sending commands to the TurtlBot of type "twist"
+
+		//stop the TurtleBot before the loop begins
+		geometry_msgs::Twist base_cmd;
+		base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
+
+		while (ros::ok()) {
+			if (turtle_move_command >= 1)
+			{
+				base_cmd.linear.x = 0.20;
+			} else if (turtle_move_command == 0){
+				base_cmd.linear.x  = 0;
+			}
+
+			//publish the assembled command and wait
+			cmd_vel_pub_.publish(base_cmd);
+			usleep(microseconds);
+
+			ros::spinOnce();
+		}
+		return true;
+	}
+};
+
+//The function saves the topic data to a global variable, which will be used inside of the class
+void turtleMove(const std_msgs::Int32::ConstPtr& asus_msg_turtle)
+{
+	turtle_move_command = asus_msg_turtle->data;
+	cout << "The topic has been read" << endl;
+}
+
+int main(int argc, char** argv)
+{
+	//init the ROS node
+	ros::init(argc, argv, "robot_driver");
+	ros::NodeHandle nh;
+	ros::Subscriber cmd_status_sub = nh.subscribe("/turtleCommands", 10, turtleMove);
+
+	RobotDriver driver(nh);
+	
+	//Call out the driveTurtle function from the class
+	driver.driveTurtle();
+}
---
+++ Project/sensing_system/src/thermal.cpp
@@ -0,0 +1,162 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32MultiArray.h"
+#include "std_msgs/Float32MultiArray.h"
+
+#include <string>
+#include <sstream>
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+
+#include <time.h> //for testing speed
+
+using namespace std;
+using namespace cv;
+
+//Variables to save and compare detected human blobs between two frames
+vector<int> human;		
+vector<int> human_prev;
+//Variables to save and compare bounding boxes of detected human blobs between two frames
+vector<Rect> boundRect;
+vector<Rect> boundRect_prev;
+//Array, saving information about detected human's starting position in the image and its distance from the sensing system
+vector<float> human_distance;
+
+//Function saves information from the /humanDistance topic, published by the depth sensor
+void callbackDist(const std_msgs::Float32MultiArray::ConstPtr& msgs)
+{
+	human_distance.clear();
+	for (int i = 0; i < msgs->data.size(); i++)
+	{
+		human_distance[i] = msgs->data[i];
+	}
+}
+
+int main(int argc, char **argv)
+{
+	clock_t start, end; //for testing
+	Mat frame;
+	Scalar color = Scalar(0, 0, 255); //red
+	Scalar color1 = Scalar(255, 0, 0); //blue
+	std_msgs::Int32MultiArray msg;
+	//stringstream name;
+
+	ros::init(argc, argv, "thermal_node_cpp");
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("/humanDistance", 10, callbackDist);
+	ros::Publisher chatter_pub = n.advertise<std_msgs::Int32MultiArray>("/thermalHumans", 10);
+
+	ros::Rate loop_rate(10);
+
+	//Capturing image form the thermal camera
+	VideoCapture cap("http://169.254.228.255/mjpg/video.mjpg?");
+
+	if (!cap.isOpened())
+	{
+	cout << "Camera not found" << endl;
+	getchar();
+	return -1;
+	}
+
+	while (cap.isOpened())
+	{
+		start = clock();
+		human_prev = human;
+		human.erase(human.begin(), human.end());
+		
+		cap >> frame;
+		//Initial imae processing: thresholding, applying median blur and a morphology function - opening
+		cvtColor(frame, frame, CV_BGR2GRAY); 
+		threshold(frame, frame, 40, 255, THRESH_BINARY);
+		medianBlur(frame, frame, 5);
+		morphologyEx(frame, frame, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(11, 11)));
+
+		//Vector to save point of the detected contours (blobs)
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		findContours(frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+	
+		//Converting the image back to colour, so that colourfull markers could be drawn
+		cvtColor(frame, frame, CV_GRAY2RGB); 
+	
+		//Saving information about bounding boxes from a previous frame and preparing vector for new data
+		if (boundRect.size() > 0) {
+			boundRect_prev = boundRect;
+			boundRect.clear();
+		}
+
+		vector<vector<Point> > contours_poly(contours.size());
+
+		//The loop creates bounding boxes for the detected blobs and checks for human-like qualities, such as
+		//appropriate bounding box ratio and minimal pixel value, as well as draws the contours
+		for (int i = 0; i < contours.size(); i=hierarchy[i][0])
+		{
+			approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true); 
+			boundRect.push_back(boundingRect(Mat(contours_poly[i])));
+			float compare = (float)boundRect[i].width / (float)boundRect[i].height;
+			if ((0.2 <= compare) && (compare <= 0.7) && (boundRect[i].width >= 45) && (boundRect[i].height >= 100)) {
+				human.push_back(i);
+			}
+			drawContours(frame, contours, i, color, 2, 8, hierarchy, 0, Point());
+		}
+
+		//The loop tries to find humans, identified in a previous frame, which may have gotten further/closer from
+		//the sensing system
+		for (int n = 0; n < human_prev.size(); n++)
+		{
+			for (int i = 0; i < boundRect.size(); i++) {
+				float compare = (float)boundRect[i].width / (float)boundRect_prev[human_prev.at(n)].width;
+				float compare2 = (float)boundRect[i].height / (float)boundRect_prev[human_prev.at(n)].height;
+				if ((0.8 <= compare) && (compare <= 1.5) && (0.8 <= compare2) && (compare2 <= 1.5)) {
+					human.push_back(i);
+				}
+			}
+		}
+
+		msg.data.clear();
+		vector <int> tempBoundRectIndex;
+		//Send positions of detected humans to the Asus sensor, so that it can estimate distance to them
+		for (int j = 0; j < human.size(); j++)
+		{
+			if (ros::ok())
+			{
+				int begin = boundRect[human[j]].x;
+				int bend = boundRect[human[j]].x + boundRect[human[j]].width;
+				msg.data.push_back(begin);
+				msg.data.push_back(bend);
+			}
+		}
+
+		chatter_pub.publish(msg);
+		loop_rate.sleep();
+		ros::spinOnce();
+
+		int temp = 0;
+		for (int j = 0; j < human_distance.size(); j + 2)
+		{
+			stringstream name;
+			if (human_distance[j] == 0)
+			{
+				name << "Human. Distance: N/A";
+			}
+			else { name << "Human. Distance: " << human_distance[j]; };
+			putText(frame, name.str(), Point(human_distance[j+1] - 10, msg.data[j+1] - 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255));
+			//might give an error regarding vector dimension
+			rectangle(frame, boundRect[human[temp]].tl(), boundRect[human[temp]].br(), color1, 2, 8, 0);
+			temp++;
+		}
+
+		imshow("Thermal blobs", frame);
+		if (frame.empty()) break;
+		end = clock();
+		std::cout << "Time required for execution: " << (double)(end - start) / CLOCKS_PER_SEC << " seconds." << std::endl;
+		if (waitKey(10) >= 0) break;
+	}
+	return 0;
+}
